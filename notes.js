215ec7e3250c79920378d37622fe30efca708946
Faryal Ansari <faryalansari@live.com>
2021-02-09 01:22:22 -0500

    add notes for review

--- notes.js
+++ notes.js
@@ -1,5 +1,15 @@
 // for flipping cards
 
+// if (gameCard[cardIndex].flipped === false) {
+  //   console.log('true')
+  //   flipFront(cardIndex)
+  //   gameCard[cardIndex].flipped = true
+  // } else {
+  //   console.log('false')
+  //   flipBack(cardIndex)
+  //   gameCard[cardIndex].flipped = false
+  // }
+
 // const flipFront = (cardIndex) => {
 //   console.log(cardIndex)
 //   card.style.width = '100%'
