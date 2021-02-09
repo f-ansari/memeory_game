b8521b725a53a20e870a427476a27fe45e22c85e
Faryal Ansari <faryalansari@live.com>
2021-02-09 01:19:07 -0500

    add images and notes.js file

---
+++ notes.js
@@ -0,0 +1,18 @@
+// for flipping cards
+
+// const flipFront = (cardIndex) => {
+//   console.log(cardIndex)
+//   card.style.width = '100%'
+//   card.style.height = '100%'
+//   card.src = gameCard[cardIndex].img
+//   card.id = cardIndex
+//   cell.appendChild(card)
+// }
+
+// const flipBack = (cardIndex) => {
+//   console.log(cardIndex)
+//   card.style.width = '100%'
+//   card.src = '/game_images/card_back.JPEG'
+//   card.id = cardIndex
+//   cell.appendChild(card)
+// }
