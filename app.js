0d10d26f4087ce7bb3aa67c7cc8ed800a3b31696
Faryal Ansari <faryalansari@live.com>
2021-02-09 01:30:03 -0500

    refactor flipCard function

--- app.js
+++ app.js
@@ -18,24 +18,22 @@ let card = document.createElement('img')
 /*Functions*/
 const handleClick = (event) => {
   const cardIndex = event.target.id
-  
+
   flipCard(cardIndex)
 }
 
 const flipCard = (cardIndex) => {
   card.style.width = '100%'
   card.style.height = '100%'
+  card.id = cardIndex
   if (gameCard[cardIndex].flipped == false) {
     gameCard[cardIndex].flipped = true
     card.src = gameCard[cardIndex].img
-    card.id = cardIndex
     cell.appendChild(card)
   } else {
     gameCard[cardIndex].flipped = false
     card.src = '/game_images/card_back.JPEG'
-    card.id = cardIndex
     cell.appendChild(card)
-    console.log('else')
   }
 }
 
