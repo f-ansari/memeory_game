079dd29bd46a74c615b466bc45aea76cfea4d06d
Faryal Ansari <faryalansari@live.com>
2021-02-11 00:03:24 -0500

    it works

--- app.js
+++ app.js
@@ -1,79 +1,73 @@
 /*********/
 /*Global Variables*/
 let isGameActive = true
+const gameBoard = document.querySelector('.game-board')
 let gameCard = [
   {
     name: 'bfwse',
-    img: '/game_images/bfwse.JPEG',
-    flipped: false
-  },
-  {
-    name: 'bfwse',
-    img: '/game_images/bfwse.JPEG',
-    flipped: false
+    img: 'game_images/bfwse.JPEG'
   },
   {
     name: 'halo',
-    img: '/game_images/halo.JPEG',
-    flipped: false
-  },
-  {
-    name: 'halo',
-    img: '/game_images/halo.JPEG',
-    flipped: false
+    img: 'game_images/halo.JPEG'
   }
 ]
-
 /*********/
 /*Functions*/
+const createBoard = () => {
+  let copy1 = gameCard
+  let matchArr = gameCard.concat(copy1)
+  gameCard = shuffle(matchArr)
+  gameCard.forEach((obj, index) => {
+    let div = document.createElement('div')
+    div.className = 'cell'
+    div.id = `${index}`
+    gameBoard.appendChild(div)
+  })
+}
+const shuffle = (arr) => {
+  let shuffleArr = arr
+  let currentIndex = arr.length
+  let temporaryValue, randomIndex
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = shuffleArr[currentIndex]
+    shuffleArr[currentIndex] = shuffleArr[randomIndex]
+    shuffleArr[randomIndex] = temporaryValue
+  }
+  return shuffleArr
+}
 const handleClick = (event) => {
-  console.log('handle click')
   const cardIndex = event.target.id
-
   flipCard(cardIndex)
 }
 
 let cardsPicked = []
 let cardIndexes = []
-
 const flipCard = (cardIndex) => {
-  console.log('flip card')
   cell = document.getElementById(cardIndex)
   card = document.createElement('img')
-
   /*Style of Card */
   card.id = cardIndex
   card.style.width = '100%'
   card.style.height = '100%'
   card.style.borderRadius = '10px solid white'
 
-  if (gameCard[cardIndex].flipped === false) {
-    gameCard[cardIndex].flipped = true
-    card.src = gameCard[cardIndex].img
-    cell.appendChild(card)
-    cardsPicked.push(gameCard[cardIndex].img)
-    cardIndexes.push(cardIndex)
-    if (cardsPicked.length === 2) {
-      setTimeout(() => {
-        checkMatch()
-      }, 900)
-    }
-  } else if (gameCard[cardIndex].flipped === true) {
-    cardsPicked.pop()
-    cardIndexes.pop()
-    cell.removeChild(cell.lastElementChild)
-    gameCard[cardIndex].flipped = false
+  card.src = gameCard[cardIndex].img
+  cell.appendChild(card)
+  cardsPicked.push(gameCard[cardIndex].img)
+  cardIndexes.push(cardIndex)
+  if (cardsPicked.length === 2) {
+    setTimeout(() => {
+      checkMatch()
+    }, 900)
   }
-  
 }
-
 cardsWonArr = []
-
 const checkMatch = () => {
-  console.log('check match')
   let card1 = document.getElementById(cardIndexes[0])
   let card2 = document.getElementById(cardIndexes[1])
-
   if (cardsPicked[0] === cardsPicked[1]) {
     console.log('its a match!')
     cardsWonArr.push(cardsPicked)
@@ -87,14 +81,14 @@ const checkMatch = () => {
     cardsPicked = []
     card1.removeChild(card1.childNodes[0])
     card2.removeChild(card2.childNodes[0])
-    gameCard[cardIndexes[0]].flipped = false
-    gameCard[cardIndexes[1]].flipped = false
   }
   cardIndexes = []
 }
-
 /*********/
 /*Event Listeners*/
-document.querySelectorAll('.cell').forEach(function (cell) {
-  cell.addEventListener('click', handleClick)
+document.addEventListener('DOMContentLoaded', () => {
+  createBoard()
+  document.querySelectorAll('.cell').forEach(function (cell) {
+    cell.addEventListener('click', handleClick)
+  })
 })
--- notes.js
+++ notes.js
@@ -1,14 +1,14 @@
 // for flipping cards
 
 // if (gameCard[cardIndex].flipped === false) {
-  //   console.log('true')
-  //   flipFront(cardIndex)
-  //   gameCard[cardIndex].flipped = true
-  // } else {
-  //   console.log('false')
-  //   flipBack(cardIndex)
-  //   gameCard[cardIndex].flipped = false
-  // }
+//   console.log('true')
+//   flipFront(cardIndex)
+//   gameCard[cardIndex].flipped = true
+// } else {
+//   console.log('false')
+//   flipBack(cardIndex)
+//   gameCard[cardIndex].flipped = false
+// }
 
 // const flipFront = (cardIndex) => {
 //   console.log(cardIndex)
@@ -26,3 +26,23 @@
 //   card.id = cardIndex
 //   cell.appendChild(card)
 // }
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8]
+// let easy1 = num.slice(0,4)
+// let easy2 = easy1
+// let matchArr = easy1.concat(easy2)
+
+// const shuffle = (arr) => {
+//   let shuffleArr = arr
+//   let currentIndex = arr.length
+//   let temporaryValue, randomIndex;
+//   while (currentIndex !== 0) {
+//     randomIndex = Math.floor(Math.random() * currentIndex)
+//     currentIndex -= 1
+//     temporaryValue = shuffleArr[currentIndex]
+//     shuffleArr[currentIndex] = shuffleArr[randomIndex]
+//     shuffleArr[randomIndex] = temporaryValue
+//   }
+//   return shuffleArr
+// }
+// console.log(shuffle(matchArr))
